9952b166236954bd1fb24b5666e14c867ff3f0a3
atbuy <buy@atbuy.dev>
2024-12-21 20:41:27 +0200

    Moved devicons dependency in nvim-tree

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -10,6 +10,9 @@ end
 
 return {
 	"nvim-tree/nvim-tree.lua",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
 	config = function()
 		local nvim_tree = require("nvim-tree")
 		nvim_tree.setup({
