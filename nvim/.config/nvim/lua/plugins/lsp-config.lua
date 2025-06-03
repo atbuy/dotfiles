aba757f04b041b1a830e5fbdfc75109e7391eba1
atbuy <buy@atbuy.dev>
2025-06-03 08:18:19 +0300

    Improve speed of pyright

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,183 +1,198 @@
 return {
-	{
-		"ray-x/lsp_signature.nvim",
-		event = "VeryLazy",
-		opts = {
-			bind = true,
-			handler_opts = { border = "rounded" },
-			hint_enable = false,
-		},
-		config = function(_, opts)
-			require("lsp_signature").setup(opts)
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "VeryLazy",
+    opts = {
+      bind = true,
+      handler_opts = { border = "rounded" },
+      hint_enable = false,
+    },
+    config = function(_, opts)
+      require("lsp_signature").setup(opts)
 
-			-- Change color of highlight group
-			vim.cmd("highlight LspSignatureActiveParameter guifg=#ff3131")
-		end,
-	},
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			local mason_packages = {
-				-- Python
-				"pyright",
-				"ruff",
-				"debugpy",
-				"mypy",
-				"taplo", -- TOML
+      -- Change color of highlight group
+      vim.cmd("highlight LspSignatureActiveParameter guifg=#ff3131")
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      local mason_packages = {
+        -- Python
+        "pyright",
+        "ruff",
+        "debugpy",
+        "mypy",
+        "taplo", -- TOML
 
-				-- Lua
-				"stylua",
+        -- Lua
+        "stylua",
 
-				-- Go
-				"gopls",
-				"golines",
-				"gofumpt",
-				"goimports-reviser",
+        -- Go
+        "gopls",
+        "golines",
+        "gofumpt",
+        "goimports-reviser",
 
-				-- Rust
-				"rust-analyzer",
+        -- Rust
+        "rust-analyzer",
 
-				-- SystemVerilog
-				-- "svls",
+        -- SystemVerilog
+        -- "svls",
 
-				-- Javascript
-				"prettier",
-				"eslint_d",
+        -- Javascript
+        "prettier",
+        "eslint_d",
 
-				-- Zig
-				"zls",
-			}
+        -- Zig
+        "zls",
+      }
 
-			require("mason").setup({
-				ensure_installed = mason_packages,
-			})
+      require("mason").setup({
+        ensure_installed = mason_packages,
+      })
 
-			-- Create MasonInstallAll command
-			vim.api.nvim_create_user_command("MasonInstallAll", function()
-				vim.cmd("MasonInstall " .. table.concat(mason_packages, " "))
-			end, {})
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					-- Lua
-					"lua_ls",
+      -- Create MasonInstallAll command
+      vim.api.nvim_create_user_command("MasonInstallAll", function()
+        vim.cmd("MasonInstall " .. table.concat(mason_packages, " "))
+      end, {})
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          -- Lua
+          "lua_ls",
 
-					-- Web
-					"ts_ls",
-					"html",
-					"cssls",
-					"eslint",
-					"emmet_ls",
-					"tailwindcss",
+          -- Web
+          "ts_ls",
+          "html",
+          "cssls",
+          "eslint",
+          "emmet_ls",
+          "tailwindcss",
 
-					-- Backend
-					"ruff",
-					"gopls",
-					"phpactor",
+          -- Backend
+          "ruff",
+          "gopls",
+          "phpactor",
 
-					-- Low level
-					"rust_analyzer",
-					-- "clangd",
+          -- Low level
+          "rust_analyzer",
+          -- "clangd",
 
-					-- SQL
-					-- "sqlls",
+          -- SQL
+          -- "sqlls",
 
-					-- Make
-					"autotools_ls",
+          -- Make
+          "autotools_ls",
 
-					-- SystemVerilog
-					-- "svls",
+          -- SystemVerilog
+          -- "svls",
 
-					-- Docker
-					"dockerls",
-					"docker_compose_language_service",
+          -- Docker
+          "dockerls",
+          "docker_compose_language_service",
 
-					-- Java
-					-- "kotlin_language_server",
+          -- Java
+          -- "kotlin_language_server",
 
-					-- Others
-					"jsonls",
-					"yamlls",
-					"taplo",
-					-- "powershell_es",
-				},
-				automatic_installation = true,
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({ capabilities = capabilities })
-			lspconfig.ts_ls.setup({ capabilities = capabilities })
-			lspconfig.html.setup({ capabilities = capabilities })
-			lspconfig.cssls.setup({ capabilities = capabilities })
-			lspconfig.eslint.setup({ capabilities = capabilities })
-			lspconfig.emmet_ls.setup({
-				capabilities = capabilities,
-				filetypes = {
-					"html",
-					"css",
-					"scss",
-					"less",
-					"sass",
-					"javascript",
-					"javascriptreact",
-					"typescript",
-					"typescriptreact",
-					"php",
-				},
-			})
-			lspconfig.tailwindcss.setup({ capabilities = capabilities })
-			-- lspconfig.svls.setup({ capabilities = capabilities })
-			lspconfig.pyright.setup({
-				capabilities = capabilities,
-				settings = {
-					python = {
-						analysis = {
-							typeCheckingMode = "off",
-						},
-					},
-				},
-			})
-			lspconfig.ruff.setup({
-				capabilities = capabilities,
-				filetypes = { "python" },
-			})
-			lspconfig.gopls.setup({ capabilities = capabilities })
-			lspconfig.phpactor.setup({ capabilities = capabilities })
-			lspconfig.rust_analyzer.setup({ capabilities = capabilities })
-			lspconfig.clangd.setup({ capabilities = capabilities })
-			lspconfig.sqlls.setup({ capabilities = capabilities })
-			lspconfig.autotools_ls.setup({ capabilities = capabilities })
-			lspconfig.dockerls.setup({ capabilities = capabilities })
-			lspconfig.docker_compose_language_service.setup({ capabilities = capabilities })
-			lspconfig.kotlin_language_server.setup({ capabilities = capabilities })
-			lspconfig.jsonls.setup({ capabilities = capabilities })
-			lspconfig.yamlls.setup({ capabilities = capabilities })
-			lspconfig.taplo.setup({ capabilities = capabilities })
-			lspconfig.powershell_es.setup({ capabilities = capabilities })
-			lspconfig.zls.setup({ capabilities = capabilities })
+          -- Others
+          "jsonls",
+          "yamlls",
+          "taplo",
+          -- "powershell_es",
+        },
+        automatic_installation = true,
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({ capabilities = capabilities })
+      lspconfig.ts_ls.setup({ capabilities = capabilities })
+      lspconfig.html.setup({ capabilities = capabilities })
+      lspconfig.cssls.setup({ capabilities = capabilities })
+      lspconfig.eslint.setup({ capabilities = capabilities })
+      lspconfig.emmet_ls.setup({
+        capabilities = capabilities,
+        filetypes = {
+          "html",
+          "css",
+          "scss",
+          "less",
+          "sass",
+          "javascript",
+          "javascriptreact",
+          "typescript",
+          "typescriptreact",
+          "php",
+        },
+      })
+      lspconfig.tailwindcss.setup({ capabilities = capabilities })
+      -- lspconfig.svls.setup({ capabilities = capabilities })
+      lspconfig.pyright.setup({
+        capabilities = capabilities,
+        on_new_config = function(cfg, root)
+          print(root)
+        end,
+        root_dir = function()
+          return vim.fn.getcwd()
+        end,
+        settings = {
+          pyright = {
+            disableOrganizeImports = true,
+          },
+          python = {
+            analysis = {
+              autoSearchPaths = true,
+              useLibraryCodeForTypes = true,
+              diagnosticMode = "openFilesOnly",
 
-			-- Keymaps
-			require("keymaps.lspconfig")
-		end,
-		opts = {
-			servers = {
-				svls = {
-					root_dir = function(_)
-						return require("lspconfig.util").find_git_ancestor
-					end,
-					cmd = { "svls" },
-					filetypes = { "verilog", "systemverilog" },
-				},
-			},
-		},
-	},
+              -- Only use Ruff for linting
+              ignore = { "*" },
+              typeCheckingMode = "off",
+            },
+          },
+        },
+      })
+      lspconfig.ruff.setup({
+        capabilities = capabilities,
+        filetypes = { "python" },
+      })
+      lspconfig.gopls.setup({ capabilities = capabilities })
+      lspconfig.phpactor.setup({ capabilities = capabilities })
+      lspconfig.rust_analyzer.setup({ capabilities = capabilities })
+      lspconfig.clangd.setup({ capabilities = capabilities })
+      lspconfig.sqlls.setup({ capabilities = capabilities })
+      lspconfig.autotools_ls.setup({ capabilities = capabilities })
+      lspconfig.dockerls.setup({ capabilities = capabilities })
+      lspconfig.docker_compose_language_service.setup({ capabilities = capabilities })
+      lspconfig.kotlin_language_server.setup({ capabilities = capabilities })
+      lspconfig.jsonls.setup({ capabilities = capabilities })
+      lspconfig.yamlls.setup({ capabilities = capabilities })
+      lspconfig.taplo.setup({ capabilities = capabilities })
+      lspconfig.powershell_es.setup({ capabilities = capabilities })
+      lspconfig.zls.setup({ capabilities = capabilities })
+
+      -- Keymaps
+      require("keymaps.lspconfig")
+    end,
+    opts = {
+      servers = {
+        svls = {
+          root_dir = function(_)
+            return require("lspconfig.util").find_git_ancestor
+          end,
+          cmd = { "svls" },
+          filetypes = { "verilog", "systemverilog" },
+        },
+      },
+    },
+  },
 }
