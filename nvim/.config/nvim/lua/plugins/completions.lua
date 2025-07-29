6be12f76592169d4f133d5c55e61e6be619acc6a
atbuy <buy@atbuy.dev>
2025-07-29 04:12:39 +0300

    Setup completions to use blink.cmp

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -1,111 +1,65 @@
-local function border(hl_name)
-	return {
-		{ "╭", hl_name },
-		{ "─", hl_name },
-		{ "╮", hl_name },
-		{ "│", hl_name },
-		{ "╯", hl_name },
-		{ "─", hl_name },
-		{ "╰", hl_name },
-		{ "│", hl_name },
-	}
-end
-
 return {
-	{ "hrsh7th/cmp-nvim-lua" },
-	{ "hrsh7th/cmp-nvim-lsp" },
-	{ "hrsh7th/cmp-buffer" },
-	{ "hrsh7th/cmp-path" },
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
+	"saghen/blink.cmp",
+	dependencies = {
+		{ "rafamadriz/friendly-snippets" },
+		{ "Kaiser-Yang/blink-cmp-git" },
+		{ "Kaiser-Yang/blink-cmp-dictionary" },
+	},
+	version = "1.*",
+	opts = {
+		keymap = {
+			preset = "none",
+			["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+			["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+			["<CR>"] = { "accept", "fallback" },
 		},
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		config = function()
-			local cmp = require("cmp")
-			require("luasnip.loaders.from_vscode").lazy_load()
-
-			cmp.setup.filetype({ "sql" }, {
-				sources = {
-					{ name = "vim-dadbod-completion" },
-					{ name = "buffer" },
+		appearance = {
+			nerd_font_variant = "mono",
+		},
+		completion = {
+			accept = { auto_brackets = { enabled = true } },
+			ghost_text = {
+				enabled = false,
+				show_with_menu = true,
+			},
+			menu = {
+				auto_show = true,
+				border = "rounded",
+				winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
+			},
+			keyword = { range = "full" },
+			list = {
+				selection = {
+					preselect = false,
+					auto_insert = true,
 				},
-			})
-
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
+			},
+			trigger = {
+				show_on_trigger_character = true,
+				show_in_snippet = false,
+			},
+			documentation = { auto_show = true, window = { border = "rounded" } },
+		},
+		sources = {
+			default = { "git", "dictionary", "lsp", "path", "snippets", "buffer" },
+			providers = {
+				git = {
+					module = "blink-cmp-git",
+					name = "Git",
+					enabled = function()
+						return vim.tbl_contains({ "octo", "gitcommit", "markdown" }, vim.bo.filetype)
 					end,
+					opts = {},
 				},
-				completeopt = "noselect",
-				preselect = cmp.PreselectMode.None,
-				window = {
-					completion = {
-						border = border("CmpBorder"),
-						side_padding = 0,
-						-- winhighlight = "Normal:CmpPmenu,CursorLine:CmpSel,Search:PmenuSel",
-						scrollbar = false,
-					},
-					documentation = {
-						border = border("CmpDocBorder"),
-						-- winhighlight = "Normal:CmpDoc",
-					},
+				dictionary = {
+					module = "blink-cmp-dictionary",
+					name = "Dictionary",
+					min_keyword_length = 3,
+					opts = {},
 				},
-				mapping = {
-					["<C-p>"] = cmp.mapping.select_prev_item(),
-					["<C-n>"] = cmp.mapping.select_next_item(),
-					["<C-d>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.close(),
-					["<CR>"] = cmp.mapping.confirm({
-						behavior = cmp.ConfirmBehavior.Insert,
-						select = false,
-					}),
-					["<Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_next_item()
-						elseif require("luasnip").expand_or_jumpable() then
-							vim.fn.feedkeys(
-								vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true),
-								""
-							)
-						else
-							fallback()
-						end
-					end, {
-						"i",
-						"s",
-					}),
-					["<S-Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_prev_item()
-						elseif require("luasnip").jumpable(-1) then
-							vim.fn.feedkeys(
-								vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true),
-								""
-							)
-						else
-							fallback()
-						end
-					end, {
-						"i",
-						"s",
-					}),
-				},
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-					{ name = "buffer" },
-					{ name = "nvim_lua" },
-					{ name = "path" },
-				}),
-			})
-		end,
+			},
+		},
+		fuzzy = { implementation = "prefer_rust" },
 	},
+	opts_extend = { "sources.default" },
 }
