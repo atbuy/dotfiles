f8490e0ef95f1d64100e2a237e728b7980898418
atbuy <buy@atbuy.dev>
2025-09-24 14:58:47 +0300

    Updated polybar to be different for main screen

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -2,9 +2,15 @@
 
 killall -q polybar
 
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --config=$HOME/.config/polybar/config.ini --reload=toph &
+    if [ "$m" = "HDMI-1" ]; then
+      MONITOR=$m polybar --config=$HOME/.config/polybar/config.ini --reload=toph &
+    else
+      MONITOR=$m polybar --config=$HOME/.config/polybar/config-other.ini --reload=toph &
+    fi
   done
 else
   polybar --config=$HOME/.config/polybar/config.ini --reload=toph &
