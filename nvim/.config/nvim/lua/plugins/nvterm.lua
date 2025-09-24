a6fdadb188c1c77435ade72b3c7aefcc781f6765
atbuy <buy@atbuy.dev>
2025-09-24 11:42:34 +0300

    Improved lazy loading for nvterm

--- nvim/.config/nvim/lua/plugins/nvterm.lua
+++ nvim/.config/nvim/lua/plugins/nvterm.lua
@@ -1,7 +1,12 @@
 return {
   "NvChad/nvterm",
+  keys = {
+    { "<A-i>", function() require("nvterm.terminal").toggle("float") end,      mode = { "n", "t" }, desc = "Toggle floating terminal" },
+    { "<A-h>", function() require("nvterm.terminal").toggle("horizontal") end, mode = { "n", "t" }, desc = "Toggle horizontal terminal" },
+    { "<A-v>", function() require("nvterm.terminal").toggle("vertical") end,   mode = { "n", "t" }, desc = "Toggle vertical terminal" },
+  },
   config = function()
     require("nvterm").setup()
-    require("keymaps.nvterm")
   end,
+
 }
