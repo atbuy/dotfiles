ca0b4a8d475a6ec7bb3c6fb0df65b31e97dd0196
atbuy <buy@atbuy.dev>
2025-09-24 13:04:56 +0300

    Fixed typos

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -1,8 +1,8 @@
 return {
   "numToStr/Comment.nvim",
   keys = {
-    { "<leader>/", function() require("Comment.api").toggle.linewise.current() end,             modes = { "n" }, desc = "Toggle comment for line" },
-    { "<leader>/", "<Esc>:lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>", modes = { "v" }, desc = "Toggle comment for selection" },
+    { "<leader>/", function() require("Comment.api").toggle.linewise.current() end,             mode = { "n" }, desc = "Toggle comment for line" },
+    { "<leader>/", "<Esc>:lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>", mode = { "v" }, desc = "Toggle comment for selection" },
   },
   config = function()
     require("Comment").setup({
@@ -12,7 +12,5 @@ return {
         extra = false,
       },
     })
-
-    require("keymaps.comment")
   end,
 }
