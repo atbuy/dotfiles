546fba65bf9ff14997f82c73810d972dd673727d
atbuy <buy@atbuy.dev>
2025-09-24 12:03:17 +0300

    Improved lazy loading for telescope-ui-select

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -29,37 +29,37 @@ return {
     tag = "0.1.8",
     dependencies = {
       { "nvim-lua/plenary.nvim" },
-      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" }
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+      {
+        "nvim-telescope/telescope-ui-select.nvim",
+        config = function()
+          local telescope = require("telescope")
+
+          telescope.setup({
+            extensions = {
+              ["ui-select"] = {
+                require("telescope.themes").get_dropdown({}),
+              },
+            },
+            pickers = {
+              live_grep = {
+                additional_args = function(_)
+                  return { "--hidden", "--no-ignore" }
+                end,
+                file_ignore_patterns = { "node_modules", ".git", ".venv" },
+              },
+              find_files = {
+                find_command = { "fd", "--type", "f", "--hidden", "--no-ignore" },
+                file_ignore_patterns = { "node_modules", ".git", ".venv" },
+                hidden = true,
+              }
+            }
+          })
+
+          telescope.load_extension("ui-select")
+          telescope.load_extension("fzf")
+        end,
+      }
     },
-  },
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-    config = function()
-      local telescope = require("telescope")
-
-      telescope.setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-          },
-        },
-        pickers = {
-          live_grep = {
-            additional_args = function(_)
-              return { "--hidden", "--no-ignore" }
-            end,
-            file_ignore_patterns = { "node_modules", ".git", ".venv" },
-          },
-          find_files = {
-            find_command = { "fd", "--type", "f", "--hidden", "--no-ignore" },
-            file_ignore_patterns = { "node_modules", ".git", ".venv" },
-            hidden = true,
-          }
-        }
-      })
-
-      telescope.load_extension("ui-select")
-      telescope.load_extension("fzf")
-    end,
-  },
+  }
 }
