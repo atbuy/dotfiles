0e608e5bb3da53f71f8e9d8457727a7ecaacca36
atbuy <buy@atbuy.dev>
2025-09-24 11:42:02 +0300

    Improved lazy loading for gitsigns

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,8 +1,35 @@
 return {
   "lewis6991/gitsigns.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  keys = {
+    { "gb", function() require("gitsigns").blame_line() end,     "Git blame line" },
+    { "td", function() require("gitsigns").toggle_deleted() end, desc = "Toggle deleted lines" },
+    {
+      "]c",
+      function()
+        if vim.wo.diff then
+          vim.cmd.normal({ "]c", bang = true })
+        else
+          require("gitsigns").nav_hunk("next")
+        end
+      end,
+      desc = "Go to next hunk"
+    },
+    {
+      "[c",
+      function()
+        if vim.wo.diff then
+          vim.cmd.normal({ "[c", bang = true })
+        else
+          require("gitsigns").nav_hunk("prev")
+        end
+      end,
+      desc = "Go to previous hunk"
+    },
+
+  },
   opts = {},
   config = function(_, opts)
     require("gitsigns").setup(opts)
-    require("keymaps.gitsigns")
   end,
 }
