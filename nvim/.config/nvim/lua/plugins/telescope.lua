d3abca8b204bab9cb7580424fac87a586be996cb
atbuy <buy@atbuy.dev>
2025-09-24 11:43:27 +0300

    Improved lazy loading for telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,31 @@
+local keys = {
+  { "<leader>ff", ":Telescope find_files<CR>",                desc = "Find files" },
+  { "<leader>fw", ":Telescope live_grep<CR>",                 desc = "Live grep" },
+  { "<leader>fb", ":Telescope buffers<CR>",                   desc = "Find buffers" },
+  { "<leader>fh", ":Telescope help_tags<CR>",                 desc = "Help page" },
+  { "<leader>fo", ":Telescope oldfiles<CR>",                  desc = "Find old files" },
+  { "<leader>fz", ":Telescope current_buffer_fuzzy_find<CR>", desc = "Find in current buffer" },
+  { "<leader>cm", ":Telescope git_commits<CR>",               desc = "View git commits" },
+  { "<leader>gt", ":Telescope git_status<CR>",                desc = "View git status" },
+  { "<leader>ma", ":Telescope marks<CR>",                     desc = "View all marks" },
+  {
+    "<leader>fa",
+    function()
+      require("telescope.builtin").find_files({
+        follow = true,
+        no_ignore = true,
+        hidden = true,
+      })
+    end,
+    desc = "Find all"
+  },
+}
+
 return {
   {
     "nvim-telescope/telescope.nvim",
+    cmd = "Telescope",
+    keys = keys,
     tag = "0.1.8",
     dependencies = {
       { "nvim-lua/plenary.nvim" },
@@ -35,8 +60,6 @@ return {
 
       telescope.load_extension("ui-select")
       telescope.load_extension("fzf")
-
-      require("keymaps.telescope")
     end,
   },
 }
