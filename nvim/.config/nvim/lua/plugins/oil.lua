1175f4dad01a469cf36742cb138def2a8898fd54
atbuy <buy@atbuy.dev>
2025-10-01 20:10:46 +0300

    Added option to move to trash instead of deleting automatically

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -11,7 +11,7 @@ return {
     config = function()
       require("oil").setup({
         default_file_explorer = true,
-        delete_to_trash = false,
+        delete_to_trash = true,
         skip_confirm_for_simple_edits = true,
 
         columns = { "icon" },
