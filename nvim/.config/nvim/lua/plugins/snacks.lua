ccf217645fa8c6859ba4bd1fc09482e04d77b653
atbuy <buy@atbuy.dev>
2026-03-23 23:05:45 +0200

    Added custom sections to dashboard

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -25,6 +25,40 @@ return {
                                    ░
         ]]
       },
+      sections = {
+        { section = "header" },
+        {
+          pane = 2,
+          section = "terminal",
+          cmd = "fortune -s | cowsay",
+          padding = 1,
+          ttl = 1,
+          pane_gap = 5,
+        },
+        { section = "keys",  gap = 1, padding = 1 },
+        {
+          pane = 2,
+          icon = " ",
+          title = "Recent Files",
+          section = "recent_files",
+          indent = 2,
+          padding = 1,
+        },
+        {
+          pane = 2,
+          icon = " ",
+          title = "Git Status",
+          section = "terminal",
+          enabled = function()
+            return Snacks.git.get_root() ~= nil
+          end,
+          cmd = "git status --short --branch --renames",
+          height = 5,
+          padding = 1,
+          indent = 3,
+        },
+        { section = "startup" },
+      },
     },
   },
   keys = {
