8e9be9198bcccb59561ff8eca3ec9c47591ca196
atbuy <buy@atbuy.dev>
2026-05-20 07:22:05 +0300

    Added status to lualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -6,6 +6,15 @@ return {
         globalstatus = true,
         theme = "tomorrow_night",
       },
+      sections = {
+        lualine_x = {
+          {
+            require("noice").api.statusline.mode.get,
+            cond = require("noice").api.statusline.mode.has,
+            color = { fg = "#ff9e64" }
+          }
+        }
+      }
     })
   end,
 }
